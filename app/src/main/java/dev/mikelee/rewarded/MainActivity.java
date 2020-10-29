a687c0f8cf4a4b6dec4d93581bc2c1bd999f60e4
Mike <mike.lee816@gmail.com>
2020-10-28 21:49:28 -0400

    Add todo and reward fragments with viewPager

--- app/src/main/java/dev/mikelee/rewarded/MainActivity.java
+++ app/src/main/java/dev/mikelee/rewarded/MainActivity.java
@@ -1,14 +1,36 @@
 package dev.mikelee.rewarded;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import dev.mikelee.rewarded.fragments.RewardFragment;
+import dev.mikelee.rewarded.fragments.TodoFragment;
+import dev.mikelee.rewarded.fragments.adapters.ViewPagerAdapter;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabs  = findViewById(R.id.tabs);
+
+        setUpTabs();
+    }
+
+    private void setUpTabs() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this.getSupportFragmentManager());
+        adapter.addFragment(new TodoFragment(), "To Do");
+        adapter.addFragment(new RewardFragment(), "Rewards");
+        viewPager.setAdapter(adapter);
+        tabs.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/dev/mikelee/rewarded/fragments/RewardFragment.java
@@ -0,0 +1,29 @@
+package dev.mikelee.rewarded.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dev.mikelee.rewarded.R;
+
+public class RewardFragment extends Fragment {
+    public RewardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reward, container, false);
+    }
+}
